1b39c7778b768ccde61f1cc92b01815f375d4301
CamiloMunozAL <Camilo.munoz2@utp.edu.co>
2024-07-13 10:22:44 -0500

    another

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('dev'));
+
+  const configService= app.get(ConfigService);
+
+  console.log(configService.get('PORT'))
+
+  app.enableCors();
+
+  // Sirve para agregar un prefijo a todas las rutas
+  app.setGlobalPrefix('api');
+  
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
